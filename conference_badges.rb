39a3531cf077743d38b9e6411a0dbaeff83f0808
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-15 19:32:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,9 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each{|badge| new_array.append(badge_maker(badge))}
+  new_array
+end
